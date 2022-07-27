9f54be3addcfb781bfcd73729a97ff2b214a7a54
Andrei Kuryshev <andrei.urich@yandex.ru>
2022-07-27 10:32:56 +0400

    Убрал анимацию на сплэше. Изменил основную тему на Material.NoActionBar

--- app/src/main/java/ru/profsoft/profnotes_v1/ui/splash/SplashActivity.kt
+++ app/src/main/java/ru/profsoft/profnotes_v1/ui/splash/SplashActivity.kt
@@ -11,20 +11,10 @@ import ru.profsoft.profnotes_v1.R
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /* val intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
 
-         */
 
-        setContentView(R.layout.activity_splash_screen)
-        val ivSplash = findViewById<ImageView>(R.id.iv_splash)
-        ivSplash.alpha = 0f
-        ivSplash.animate().setDuration(1500).alpha(1f).withEndAction {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-            finish()
-        }
     }
 }
